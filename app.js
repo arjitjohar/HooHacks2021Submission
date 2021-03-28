c3851a8c3b7d8a8d80d5964d9a0f5df60f389304
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-03-28 11:36:28 -0400

    submission final

--- app.js
+++ app.js
@@ -14,32 +14,109 @@ var Firebase = require('firebase'),
     usersRef = new Firebase('{https://moodbuddy-c5dae-default-rtdb.firebaseio.com/}/Users/');
  
 var numbers = [];
- 
+var mood = [] 
+var activated = false;
+
+
 usersRef.on('child_added', function(snapshot) {
   numbers.push( snapshot.val() );
+  
   console.log( 'Added number ' + snapshot.val() );
+  phoneNumberMoodEntry = [snapshot.val(), 0]
+  mood.push(phoneNumberMoodEntry)
 });
+
+
+function resetMood() {
+  for (i = 0; i < mood.length; i++) 
+  {
+    mood[i] = [numbers[i] , 0]
+  }
+};
  
-var textJob = new cronJob( '0 18 * * *', function(){
+var textJob = new cronJob( '*/1 * * * *', function(){
   for( var i = 0; i < numbers.length; i++ ) {
-    client.sendMessage( { to:numbers[i], from:'+16475594970', body:'Hello! Hope you’re having a good day.'}, function( err, data ) {
+    client.sendMessage( { to:numbers[i], from:'+16475594970', body:'Hello! moodBuddy wants you to know that your new week has started! remember to message moodBuddy every day how you are feeling 😊.'}, function( err, data ) {
       console.log( data.body );
+        
+
+    
+
     });
   }
 },  null, true);
+
+
+
+
+var textJob2 = new cronJob( '*/2 * * * *', function(){
+  for( var i = 0; i < numbers.length; i++ ) {
+
+    var score = mood[i]
+    if (score[1] > 5){
+
+        client.sendMessage( { to:numbers[i], from:'+16475594970', body:'Hello! This weeks mood score was a '+score[1].toString()+' you had a really good week!'}, function( err, data ) {
+          
+          console.log( data.body );
+    
+            });
+    }
+
+    else { 
+        client.sendMessage( { to:numbers[i], from:'+16475594970', body:'Hello! This weeks mood score was a '+score[1].toString()+ ' you had a really tough week :( '}, function( err, data ) {
+          
+          console.log( data.body );
+    
+            });
+    }
+  }
+  resetMood()
+},  null, true);
+
+
+
  
-app.post('/message', function (req, res) {
+
+
+app.post('/sms', function (req, res) {
   var resp = new twilio.TwimlResponse();
-  if( req.body.Body.trim().toLowerCase() === 'subscribe' ) {
+  if( req.body.Body.trim().toLowerCase() === 'subscribe' ) {W
     var fromNum = req.body.From;
     if(numbers.indexOf(fromNum) !== -1) {
-      resp.message('You already subscribed!');
+          resp.message("you are already subscribed")
     } else {
       resp.message('Thank you, you are now subscribed. Reply "STOP" to stop receiving updates.');
       usersRef.push(fromNum);
     }
   } else {
-    resp.message('Welcome to Daily Updates. Text "Subscribe" receive updates.');
+    if( req.body.Body.trim().toLowerCase() === 'happy' ) {
+      var fromNum = req.body.From
+      if(numbers.indexOf(fromNum) !== -1) {
+        resp.message("happy")
+        for (i = 0; i < mood.length; i++) {
+          if (mood[i][0] == fromNum) {
+            mood[i][1] = mood[i][1] + 1
+          }
+        }
+      }
+    }
+
+    if( req.body.Body.trim().toLowerCase() === 'sad' ) {
+      var fromNum = req.body.From
+      if(numbers.indexOf(fromNum) !== -1) {
+        resp.message("sad")
+        
+        for (i = 0; i < mood.length; i++) {
+          if (mood[i][0] == fromNum) {
+            mood[i][1] = mood[i][1] - 1
+          }
+        }
+      }
+    }
+
+    else if(req.body.Body.trim().toLowerCase() !== 'sad' || req.body.Body.trim().toLowerCase() !== "happy"  ) {
+      resp.message('Welcome to Daily Updates. Text "Subscribe" receive updates.');
+    }
   }
   res.writeHead(200, {
     'Content-Type':'text/xml'
